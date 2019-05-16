a3532f3715783324aa62d3757c9be54e910a2990
camilorico99 <kmilo9905@gmail.com>
2019-05-16 01:05:39 -0500

    proyecto sin raspberry documentado

--- Proyecto/registrarse.h
+++ Proyecto/registrarse.h
@@ -1,3 +1,11 @@
+/**
+  * @file registrarse.h
+  * @version 1.0
+  * @date 15/05/2019
+  * @authors Camilo y Nicolas
+  * @title registrarse
+  */
+
 #ifndef REGISTRARSE_H
 #define REGISTRARSE_H
 
@@ -21,18 +29,34 @@ class registrarse : public QWidget
 public:
     explicit registrarse(QWidget *parent = 0);
     ~registrarse();
-
-    void creartabla_usuario();
-
+    /**
+     * @brief insertar_usuario registra los datos de los usuarios en la base de datos
+     */
     void insertar_usuario();
 
 private slots:
+    /**
+     * @brief on_push_cancelar_clicked cancela el registro de un nuevo usuario y nos devuelve a la interfaz Proyecto
+     */
     void on_push_cancelar_clicked();
-
+    /**
+     * @brief on_push_registrar_clicked llama la funcion insertar_usuario y si los datos son correctos para registrarlos
+     * los registra y nos devuelve a la interfaz Proyecto
+     */
     void on_push_registrar_clicked();
 
 private:
+    /**
+     * @brief creartabla_usuario crea la tabla usuario y la tabla regitros en la base de datos
+     */
+    void creartabla_usuario();
+    /**
+     * @brief ui definimos un puntero a la interfaz grafica de registrarse llamado ui
+     */
     Ui::registrarse *ui;
+    /**
+     * @brief db definimos un puntero a la base de datos llamado db
+     */
     sqlite3 *db;
 };
 
--- Proyecto/ventana_principal.h
+++ Proyecto/ventana_principal.h
@@ -1,3 +1,11 @@
+/**
+  * @file ventana_principal.h
+  * @version 1.0
+  * @date 15/05/2019
+  * @authors Camilo y Nicolas
+  * @title ventana_principal
+  */
+
 #ifndef VENTANA_PRINCIPAL_H
 #define VENTANA_PRINCIPAL_H
 
@@ -19,11 +27,26 @@ public:
     ~Ventana_Principal();
 
 private slots:
+    /**
+     * @brief recibo_usuario esta funcion recibe el usuario emitido en la clase Proyecto y ejecuta hay un codigo que nos
+     * muestra los datos del usuario que ingreso a la aplicacion en la interfaz grafica, llama la funcion calcular_edad
+     * para calcular la edad de la persona que ingreso y poder mostrar este dato en la interfaz
+     */
     void recibo_usuario(QString);
-
+    /**
+     * @brief calcular_edad esta funcion calcula la edad de una persona si en la base de datos su fecha de nacimiento esta
+     * de la siquiente forma (dd-mm-aaaa)
+     * @return nos retorna la edad que tiene esta persona en el momento de ingresar a la aplicacion
+     */
     int calcular_edad(QString);
 private:
+    /**
+     * @brief ui definimos un puntero a la interfaz grafica de Ventana_Principal llamado ui
+     */
     Ui::Ventana_Principal *ui;
+    /**
+     * @brief db definimos un puntero a la base de datos llamado db
+     */
     sqlite3 *db;
 };
 
